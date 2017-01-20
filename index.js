b95bd461494af89888c0cdf6859ebd26054b13f3
Aden Naheen <elpaperboy@gmail.com>
2017-01-19 19:38:49 -0500

    updated the server file to serve a static html file

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var server = express();
 
 var port = 8080; //think of it as the door number to the server
 
+server.get('/', function(request, response){
+   response.sendFile('index.html', {root: __dirname + '/public/html'});
+
+});
 server.listen(port, function(){
 console.log('Now listening on port', port);
 });
